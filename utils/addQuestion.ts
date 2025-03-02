b8452642244663fe631375428c257498af28b913
Aki Fukushima <akifukushima@AkinoMacBook-Air.local>
2025-03-02 22:02:43 +0900

    Initial commit

---
+++ utils/addQuestion.ts
@@ -0,0 +1,15 @@
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../lib/firebase";
+
+const addQuestion = async () => {
+  await addDoc(collection(db, "questions"), {
+    question: "Macで新しいタブを開くショートカットは？",
+    options: ["Command + T", "Control + T", "Shift + T"],
+    answer: "Command + T"
+  });
+};
+
+addQuestion()
+  .then(() => console.log("クイズ追加完了"))
+  .catch((error) => console.error("エラー:", error));
+  
